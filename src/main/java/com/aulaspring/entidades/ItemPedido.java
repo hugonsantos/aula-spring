b29c9d8203cdf934a0ba920035c65b0621a3ae76
Hugo Nascimento <hugonsantos22@gmail.com>
2021-02-12 21:19:31 -0300

    Entidade ItemPedido e Associação muitos-para-muitos com dados extras parte 1

---
+++ src/main/java/com/aulaspring/entidades/ItemPedido.java
@@ -0,0 +1,89 @@
+package com.aulaspring.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import com.aulaspring.entidades.pk.ItemPedidoPK;
+
+@Entity
+public class ItemPedido implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemPedidoPK id;
+	
+	private Integer quantidade;
+	private Double price;
+	
+	public ItemPedido() {
+		
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Integer quantidade, Double price) {
+
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.price = price;
+	}
+
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setPedido(Produto produto) {
+		id.setProduto(produto);
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
