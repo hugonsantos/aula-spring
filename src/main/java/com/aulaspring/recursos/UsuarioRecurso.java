a5cb6d8ae1b5b5ac04d331f98f224d5d4e254faf
Hugo Nascimento <hugonsantos22@gmail.com>
2021-02-14 12:28:01 -0300

    Inserção de usuário

--- src/main/java/com/aulaspring/recursos/UsuarioRecurso.java
+++ src/main/java/com/aulaspring/recursos/UsuarioRecurso.java
@@ -1,13 +1,17 @@
 package com.aulaspring.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aulaspring.entidades.Usuario;
 import com.aulaspring.servicos.UsuarioServico;
@@ -31,5 +35,13 @@ public class UsuarioRecurso {
 		
 		Usuario u = servico.buscarUsuario(id);
 		return ResponseEntity.ok().body(u);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> inserirUsuario(@RequestBody Usuario usuario) {
+		
+		usuario = servico.salvar(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
+		return ResponseEntity.created(uri).body(usuario);
 	}
 }
